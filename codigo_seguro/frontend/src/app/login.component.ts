a6c0b9e3015c8a7d28362a83abf8e155447cc9cc
Beatriz Kuno <bealuz251@gmail.com>
2025-08-27 22:25:21 -0400

    codigo seguro de lista de invitados

--- codigo_seguro/frontend/src/app/login.component.ts
+++ codigo_seguro/frontend/src/app/login.component.ts
@@ -10,7 +10,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 
-
 @Component({
   selector: 'app-login',
   standalone: true,
